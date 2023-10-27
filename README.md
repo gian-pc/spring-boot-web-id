80233b22c58e94f73e864e5942231e279caa802d
Gian Paucar Cortez <77996469+gian-pc@users.noreply.github.com>
2023-10-27 15:30:42 -0500

    1. Creación del proyecto

---
+++ README.md
@@ -0,0 +1,11 @@
+                    Proyecto 2
+            (Inyección de Dependencias)
+
+1. Creación del proyecto **spring-boot-web-id**
+   - Maven
+   - Java 17
+   - Dependencias
+     - Spring Boot Actuator 
+     - Spring Web 
+     - Thymeleaf
+     - Spring Boot DevTools
---
+++ src/main/java/com/gianpc/app/SpringBootWebIdApplication.java
@@ -0,0 +1,13 @@
+package com.gianpc.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWebIdApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootWebIdApplication.class, args);
+    }
+
+}
